31f9a4a862f7fb8eddbbfb34a4e6ad30a7a1ea58
MuradAbas <161676869+MuradAbas@users.noreply.github.com>
2024-04-08 10:38:09 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+/*
+tapsiriq1
+
+for(let i = 1; i<=100; i++){
+    if (i % 2 ==0){
+        console.log(i);
+    }
+}
+*/
+
+ /*
+ tapsiriq 2
+
+let eded = +prompt("eded daxil edin");
+if(eded % 2 ==0) {
+    console.log("daxil etdiyiniz eded cutdur");
+} else{
+    console.log("daxil etdiyiniz eded tekdir");
+}
+*/
+
+/*
+tapsiriq 11
+
+let eded =+prompt("telebenin yiqdiqi bali yazin");
+if(eded>0 && eded<100){
+    if(eded >=90 ){
+        console.log("telebenin aldiqi qiymey a");
+    } else if (eded >=80 || eded <=89) {
+        console.log("telebenin aldiqi qiymet b");
+    } else if (eded>=70 || eded<=79){
+        console.log("telebenin aldiqi qiymet c");
+    }else if(eded>=60 || eded <=69) {
+        console.log("telebenin aldiqi qiymet d");
+    } else if (eded<=59){
+        console.log("telebenin aldiqi qiymet f kesildi");
+    } 
+}
+else if ("daxil etdiyiniz eded 0 100 araliqinda olmalidir")
+*/
+
+/*tapsiriq 12
+
+let eded1 = +prompt ("1 ci ededi daxil edin");
+let eded2 = +prompt ("2 ci ededin daxil edein");
+let emel =prompt ("emeli daxil edin (toplama, cixma, vurma, bolme)") 
+if (emel == "toplama "){
+    console.log(eded1+eded2);
+}else if (emel == "cixma "){
+    console.log(eded1-eded2);
+}else if (emel == "vurma "){
+    console.log(eded1*eded2);
+}else if (emel == "bolme "){
+    console.log(eded1/eded2);
+} else {
+    console.log("emeli duzgun edin");
+}*/
